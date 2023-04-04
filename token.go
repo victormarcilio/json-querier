ab7efd1b0e10d8dd26b2c300d82b09fe4b1b1d42
Victor Marcilio Peixoto <victormarcilio@gmail.com>
2023-04-03 21:00:54 -0300

    Refactor newToken

--- token.go
+++ token.go
@@ -24,30 +24,33 @@ type token struct {
 	ID       tokenID
 }
 
-func newToken(spelling string) token {
-	switch spelling {
-	case "{":
-		return token{"{", OPEN_CURLY}
-	case "}":
-		return token{"}", CLOSE_CURLY}
-	case "[":
-		return token{"[", OPEN_SQUARE}
-	case "]":
-		return token{"]", CLOSE_SQUARE}
-	case ",":
-		return token{",", COMMA}
-	case ":":
-		return token{":", SEMICOLON}
-	case "true":
-		return token{"true", TRUE}
-	case "false":
-		return token{"false", FALSE}
-	case "null":
-		return token{"null", NULL}
+var getID = func() func(string) tokenID {
+
+	spellingToID := map[string]tokenID{
+		"{":     OPEN_CURLY,
+		"}":     CLOSE_CURLY,
+		"[":     OPEN_SQUARE,
+		"]":     CLOSE_SQUARE,
+		",":     COMMA,
+		":":     SEMICOLON,
+		"true":  TRUE,
+		"false": FALSE,
+		"null":  NULL,
 	}
 
-	if _, err := strconv.ParseFloat(spelling, 64); err == nil {
-		return token{spelling, NUMBER}
+	return func(spelling string) tokenID {
+		if id, ok := spellingToID[spelling]; ok {
+			return id
+		}
+
+		if _, err := strconv.ParseFloat(spelling, 64); err == nil {
+			return NUMBER
+		}
+		return INVALID
 	}
-	return token{spelling, INVALID}
+}()
+
+func newToken(spelling string) token {
+
+	return token{spelling, getID(spelling)}
 }
